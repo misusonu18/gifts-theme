e20b1441756098415212ea3ad3b936ba90ab1eca
misusonu18 <misusonu@gmail.com>
2021-03-21 17:27:09 +0530

    changes

--- js/credential.js
+++ js/credential.js
@@ -1,8 +1,9 @@
 let email = document.getElementById('email');
 let password = document.getElementById('password');
-
 let emailValidation = document.getElementById('email-validation');
 let passwordValidation = document.getElementById('password-validation');
+let emailFlag = true;
+let passwordFlag = true;
 
 function onChangeInput(idName) {
     if (idName === 'email') {
@@ -11,32 +12,38 @@ function onChangeInput(idName) {
         let result = (email.value).match(emailRegex);
         if (email.value === '') {
             emailValidation.innerText = 'Please Enter Email Address'; 
+            emailFlag =  false;
         }
         else {
             if (!result) {
                 emailValidation.innerText = 'Invalid Email Address';
+                emailFlag =  false;
             }
             else 
                 emailValidation.innerText = '';
+                emailFlag =  true;
         }
 
     }
     else {
         if (password.value === '') {
             passwordValidation.innerText = 'Please Enter Password'; 
+            passwordFlag =  false;
         }
         else 
             passwordValidation.innerText = '';
+            passwordFlag =  true;
     }
 }
 
 function submitForm() {
-    if (email.value === '' && password.value === '') {
+    if (emailFlag === false && passwordFlag === false) {
         emailValidation.innerText = 'Please Enter Email Address'; 
         passwordValidation.innerText = 'Please Enter Password';
         console.log('ew');
     }
     else {
-        window.location.replace('index.html');
+        console.log(emailFlag);
+        // window.location.replace('index.html');
     }
 }
