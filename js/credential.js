37dca4cebe5ef0b65a47b6026e5a394d6ce8844c
misusonu18 <misusonu@gmail.com>
2021-03-25 16:01:31 +0530

    code: proper direction after login and register

--- js/credential.js
+++ js/credential.js
@@ -1,9 +1,20 @@
+let name = document.getElementById('name');
+let phoneNumber = document.getElementById('phone-number');
 let email = document.getElementById('email');
 let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
+
+let nameValidation = document.getElementById('name-validation');
+let phoneNumberValidation = document.getElementById('phone-number-validation');
 let emailValidation = document.getElementById('email-validation');
 let passwordValidation = document.getElementById('password-validation');
-let emailFlag = true;
-let passwordFlag = true;
+let confirmPasswordValidation = document.getElementById('confirm-password-validation');
+
+let emailFlag = 0;
+let passwordFlag = 0;
+let nameFlag = 0;
+let phoneNumberFlag = 0;
+let confirmPasswordFlag = 0;
 
 function onChangeInput(idName) {
     if (idName === 'email') {
@@ -12,38 +23,122 @@ function onChangeInput(idName) {
         let result = (email.value).match(emailRegex);
         if (email.value === '') {
             emailValidation.innerText = 'Please Enter Email Address'; 
-            emailFlag =  false;
+            emailFlag =  0;
         }
         else {
             if (!result) {
                 emailValidation.innerText = 'Invalid Email Address';
-                emailFlag =  false;
+                emailFlag =  0;
             }
             else 
                 emailValidation.innerText = '';
-                emailFlag =  true;
+                emailFlag =  1;
         }
 
     }
-    else {
+    if (idName === 'name') {
+        let nameRegex = /^[a-zA-Z ]{2,30}$/;
+
+        let result = (name.value).match(nameRegex);
+        if (name.value === '') {
+            nameValidation.innerText = 'Please Enter Name'; 
+            nameFlag =  0;
+        }
+        else {
+            if (!result) {
+                nameValidation.innerText = 'Invalid First Name';
+                nameFlag = 0;
+            }
+            else {
+                nameValidation.innerText = '';
+                nameFlag = 1;
+            }
+        }
+    }
+    if (idName === 'phone-number') {
+        let phoneRegex = /^[+]?(1\-|1\s|1|\d{3}\-|\d{3}\s|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/g;
+
+        let result = (phoneNumber.value).match(phoneRegex);
+        if (phoneNumber.value === '') {
+            phoneNumberValidation.innerText = 'Please Enter Phone Number';
+            phoneNumberFlag = 0
+        }
+        else {
+            if(!result) {
+                phoneNumberValidation.innerText = 'Invalid Phone Number';
+                phoneNumberFlag = 0
+            }
+            else {
+                phoneNumberValidation.innerText = '';    
+                phoneNumberFlag = 1;
+            }
+        }
+    }
+    if (idName === 'password') {
+        
         if (password.value === '') {
             passwordValidation.innerText = 'Please Enter Password'; 
-            passwordFlag =  false;
+            passwordFlag =  0;
         }
-        else 
+        else {
             passwordValidation.innerText = '';
-            passwordFlag =  true;
+            passwordFlag =  1;
+        }
+    }
+    if (idName === 'confirm-password') {
+        
+        if (confirmPassword.value === '') {
+            confirmPasswordValidation.innerText = 'Please Enter Confirm Password'; 
+            confirmPasswordFlag =  0;
+        }
+        else {
+            if (password.value === confirmPassword.value) {
+                confirmPasswordValidation.innerText = '';
+                confirmPasswordFlag =  1;
+            }
+            else {
+                confirmPasswordValidation.innerText = 'Confirm Password Does Not Match. Please Enter Proper Password';
+                confirmPasswordFlag =  0;
+            }
+        }
     }
 }
 
 function submitForm() {
-    if (emailFlag === false && passwordFlag === false) {
-        emailValidation.innerText = 'Please Enter Email Address'; 
-        passwordValidation.innerText = 'Please Enter Password';
+    if (emailFlag === 0 && passwordFlag === 0) {
+        if (emailFlag == 0) {
+            emailValidation.innerText = 'Please Enter Email Address'; 
+        }
+        if (passwordFlag == 0) {
+            passwordValidation.innerText = 'Please Enter Password';
+        }
         console.log('ew');
     }
     else {
-        console.log(emailFlag);
-        // window.location.replace('index.html');
+        window.location.replace('index.html');
+    }
+}
+
+function submitFormRegister() {
+    console.log(nameFlag + " " +  phoneNumberFlag + " " + emailFlag + " " + passwordFlag + " " + confirmPasswordFlag);
+    if (nameFlag === 0 && phoneNumberFlag === 0 && emailFlag === 0 && passwordFlag === 0 && confirmPasswordFlag === 0) {
+        if (nameFlag == 0) {
+            nameValidation.innerText = 'Please Enter Name';
+        }
+        if (phoneNumberFlag == 0) {
+            phoneNumberValidation.innerText = 'Please Enter Phone Number';
+        }
+        if (emailFlag == 0) {
+            emailValidation.innerText = 'Please Enter Email Address'; 
+        }
+        if (passwordFlag == 0) {
+            passwordValidation.innerText = 'Please Enter Password';
+        }
+        if (confirmPasswordFlag == 0) {
+            confirmPasswordValidation.innerText = 'Please Enter Confirm Password';
+        }
+    }
+    else {
+        window.location.replace('login.html');
     }
 }
